4e6355f422076bdbca12b7c945e9b911b3c0592e
Ssofja <sofiakostandian@gmail.com>
2021-09-07 17:58:34 +0400

    add file

--- article_links.py
+++ article_links.py
@@ -1,17 +1,17 @@
 from typing import List, Any
-
 import requests
 from bs4 import BeautifulSoup
 import time
 
-BASE = 'https://www.infocom.am'
 
+BASE = 'https://www.infocom.am'
+PATH = '/home/sofia/Desktop/url/Infocom/news_links.txt'
 
 
 def get_page_news_links(page):
-    bsoup = BeautifulSoup(page, features="html.parser")
+    bsoup = BeautifulSoup(page, features='html.parser')
     news_list = []
-    for a in bsoup.find_all("a", class_='infotag-page-news-item'):
+    for a in bsoup.find_all('a', class_='infotag-page-news-item'):
         news_list.append(BASE + a['href'])
     return news_list
 
@@ -22,7 +22,7 @@ def requestText(url):
         try:
             request = requests.get(url)
             if request.status_code != 200:
-                request = ""
+                request = ''
         except:
             time.sleep(5)
             continue
@@ -31,32 +31,28 @@ def requestText(url):
 
 def page_numbers(lin):
     r = requestText(lin)
-    bsoup = BeautifulSoup(r.content, features="html.parser")
-    ul = bsoup.find("ul", class_='pagination')
+    bsoup = BeautifulSoup(r.content, features='html.parser')
+    ul = bsoup.find('ul', class_='pagination')
     lis = list(ul.children)
-    if len(lis) == 1:
-        return 1
-    if len(lis) == 0:
-        return 0
+    if len(lis) < 2:
+        return len(lis)
     pages_num = int(lis[-2].text)
     return pages_num
 
 
-
- def main():
-     path = "/home/sofia/Desktop/url/Infocom/news_links.txt"
-     cat_path = "/home/sofia/Desktop/url/Infocom/catheg_links.txt"
-     all_cat_links = []
-
-    with open(cat_path, "r") as f:
+def main():
+    all_cat_links = []
+    with open(CAT_PATH, 'r') as f:
          all_cat_links = f.read().splitlines()
 
     for catheg_link in all_cat_links:
         page_num = page_numbers(catheg_link)
+
         for i in range(1, page_num + 1):
             page_link = f'{catheg_link}&p={i}'
             r = requestText(page_link)
             page_article_links = get_page_news_links(r.content)
+
             for article_link in page_article_links:
-                with open(path, "a") as f:
-                    f.write(article_link + "\n")
+                with open(PATH, 'a') as f:
+                    f.write(article_link + '\n')
--- cat.py
+++ cat.py
@@ -2,13 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+
 BASE = 'https://www.infocom.am'
+CAT_PATH = '/home/sofia/Desktop/url/Infocom/catheg_links.txt'
 
 
 def get_cathegory_links(page):
-    bsoup = BeautifulSoup(page, features="html.parser")
+    bsoup = BeautifulSoup(page, features='html.parser')
     catheg_list = []
-    for a in bsoup.find_all("a", class_='infotag-list-item'):
+    for a in bsoup.find_all('a', class_='infotag-list-item'):
         catheg_list.append(a['href'])
     return catheg_list
 
@@ -19,22 +21,21 @@ def requestText(url):
         try:
             request = requests.get(url)
             if request.status_code != 200:
-                request = ""
+                request = ''
         except:
             time.sleep(5)
             continue
     return request
 
 
-
-#dirname = "~/Desktop/url/Infocom/Covid_links.txt"
-f = open("/home/sofia/Desktop/url/Infocom/catheg_links.txt",'w')
-all_scraped_news = []
-for i in range(1, 54):
-    page_link = "https://www.infocom.am/hy/Infotags?p=i".format(i)
-    r = requestText(page_link)
-    page_catheg_links = get_cathegory_links(r.content)
-    for catheg_link in page_catheg_links:
-        print(BASE + catheg_link, file=f)
-        #print(BASE + catheg_link)
+def main():
+    all_scraped_news = []
+    for i in range(1, 54):
+        page_link = 'https://www.infocom.am/hy/Infotags?p=i'.format(i)
+        r = requestText(page_link)
+        page_categ_links = get_cathegory_links(r.content)
+        for catheg_link in page_categ_links:
+            with open(CAT_PATH, 'a') as f:
+                f.write(catheg_link + '\n')
+        
 
