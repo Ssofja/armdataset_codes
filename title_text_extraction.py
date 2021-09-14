7c234e0f249ed45a6cef350b59bbdc4981a9ad9d
Ssofja <sofiakostandian@gmail.com>
2021-09-14 16:35:42 +0400

    getting titles and texts from all links

--- title_text_extraction.py
+++ title_text_extraction.py
@@ -2,10 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import json
+import os
 
 
-url = 'https://www.infocom.am/hy/Article/63274'
-PATH = '/home/sofia/Desktop/url/Infocom/63274.json'
+
+
+ARTICLE_PATH = '/home/sofia/Desktop/url/Infocom/filtered_links.txt'
+PATH = '/home/sofia/Desktop/url/Infocom/json'
 
 
 def requestText(url):
@@ -22,23 +25,39 @@ def requestText(url):
 
 
 def main():
-    req = requestText(url)
-    bsoup = BeautifulSoup(req.content, 'html.parser')
-    title_h1 = bsoup.find('h1', class_='news-title')
-    title = title_h1.text.strip()
+    with open(ARTICLE_PATH, "r") as file1:
+        file_content = file1.read()
+        lines = file_content.splitlines()
+        for id,line in enumerate(lines):
 
-    divs = bsoup.find('div', class_='col-sm col-md-7 offset-md-1')
-    children_divs = divs.findChildren('div')
-    texts_aray = ''
-    for children_div in children_divs:
-        txt_div = children_div.findChildren('div')
-        if len(txt_div) > 0:
-            text = txt_div[0].text
-            texts_aray += text + '\n'
+            req = requestText(line)
+            bsoup = BeautifulSoup(req.content, 'html.parser')
+            title_h1 = bsoup.find('h1', class_='news-title')
+            title = title_h1.text.strip()
+            divs = bsoup.find('div', class_='col-sm col-md-7 offset-md-1')
+            children_divs = divs.findChildren('div')
+            texts_aray = ''
+            if len(children_divs):
+                for children_div in children_divs:
+                    txt_div = children_div.findChildren('div')
+                    if len(txt_div) > 0:
+                        text = txt_div[0].text
+                        texts_aray += text + '\n'
+            else:
+                array_p = divs.find('p')
+                for children_p in divs:
+                    try:
+                        text = children_p.text
+                    except:
+                        continue
+                    if text.strip()!= '':
+                        texts_aray += children_p.text + '\n'
 
-    exe = {'Title': title, 'Text': texts_aray}
-    with open(PATH, 'a') as f:
-        json.dump(exe, f, ensure_ascii=False, indent=4)
+            exe = {'title': title, 'text': texts_aray}
+            file_name = f'article{id}'
+            with open(os.path.join(PATH, file_name), 'a') as f:
+                json.dump(exe, f, ensure_ascii=False, indent=4)
+
 
 main()
 
